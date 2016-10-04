12b56904b5f4575ae4d6d3f907e28571baeed439
Feather <featherpoirier@gmail.com>
2016-10-04 17:49:49 -0400

    Initial commit

---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const sessionLinks = () => (
+  <nav className="login-signup">
+    <Link to="/login" activeClassName="current">LOGIN</Link>
+    &nbsp;or&nbsp;
+    <Link to="/signup" activeClassName="current">SIGN UP</Link>
+  </nav>
+);
+
+const personalGreeting = (currentUser, logout) => (
+	<hgroup className="header-group">
+		<h2 className="header-name">Hi, {currentUser.username}!</h2>
+		<button className="header-button" onClick={logout}>Log Out</button>
+	</hgroup>
+);
+
+const Greeting = ({ currentUser, logout }) => {
+  if (currentUser) {
+    return personalGreeting(currentUser, logout);
+  } else {
+    return sessionLinks();
+  }
+}
+
+export default Greeting;
