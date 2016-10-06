75a66a436087927c5623a717b921af7f8c70a91e
Feather <featherpoirier@gmail.com>
2016-10-06 16:27:13 -0400

    Got front page modals working

--- frontend/components/front_page/front_page.jsx
+++ frontend/components/front_page/front_page.jsx
@@ -1,31 +1,97 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Modal from 'react-modal';
+import LoginForm from './login_form';
+import SignupForm from './signup_form';
+import FormStyle from './session_form_style';
 
 class FrontPage extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.openLogin = this.openLogin.bind(this);
+    this.openSignup = this.openSignup.bind(this);
+    this.closeLogin = this.closeLogin.bind(this);
+    this.closeSignup = this.closeSignup.bind(this);
+
+    this.state = {
+      loginOpen: false,
+      signupOpen: false,
+    };
+
+  }
+
+  openLogin() {
+    this.setState({loginOpen: true});
+  }
+
+  openSignup() {
+    this.setState({signupOpen: true});
+  }
+
+  closeLogin() {
+    this.setState({loginOpen: false});
+  }
+
+  closeSignup() {
+    this.setState({signupOpen: false});
+  }
+
+  loginForm() {
+    return (
+      <Modal className="login-form" style={FormStyle} isOpen={this.state.loginOpen}>
+        <button onClick={this.closeLogin}>close</button>
+        <h1>LOGGING IN</h1>
+      </Modal>
+    );
+  }
+
+  signupForm() {
+    return (
+      <Modal className="signup-form" style={FormStyle} isOpen={this.state.signupOpen}>
+        <button onClick={this.closeSignup}>close</button>
+        <h1>SIGNING IN</h1>
+      </Modal>
+    );
   }
 
   sessionLinks() {
     return (
       <nav className="login-signup">
-        <Link to="/login" activeClassName="current">login</Link>
+        <button onClick={this.openLogin}>login</button>
         &nbsp;or&nbsp;
-        <Link to="/signup" activeClassName="current">sign up</Link>
+        <button onClick={this.openSignup}>sign up</button>
       </nav>
     );
   }
 
-  render() {
+  splashPane() {
     return (
-      <div>
-        <header>
+      <span className="splash-parent">
+        <div className="splash-pane">
           <Link to="/" className="header-link"><h1>ramblr.</h1></Link>
           {this.sessionLinks()}
-        </header>
+        </div>
+      </span>
+    );
+  }
+
+  sessionForms() {
+    return (
+      <div className="session-form">
+        {this.loginForm()}
+        {this.signupForm()}
       </div>
     );
+  }
+
+  render() {
+    if (this.state.loginOpen || this.state.signupOpen) {
+      return this.sessionForms();
+    } else {
+      return this.splashPane();
+    }
   }
 
 }
---
+++ frontend/components/front_page/session_form_style.js
@@ -0,0 +1,31 @@
+const FormStyle = {
+
+  overlay : {
+
+    position        : 'fixed',
+    top             : 0,
+    left            : 0,
+    right           : 0,
+    bottom          : 0,
+    backgroundColor : 'rgba(255, 255, 255, 0.75)'
+  },
+
+  content : {
+
+    position        : 'fixed',
+    color           : '#aaa',
+    top             : '40%',
+    left            : '50%',
+    transform       : 'translate(-50%, -50%)',
+    border          : '5px solid #000',
+    padding         : '20px',
+    background      : '#eee',
+    width           : '500px',
+    maxwidth        : '100%',
+    height          : '250px',
+    maxheight       : '100%',
+  }
+
+};
+
+export default FormStyle;
