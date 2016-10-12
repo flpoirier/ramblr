dad9342faeefe45deb281e9c3dc3354f3638619b
Feather <featherpoirier@gmail.com>
2016-10-12 15:38:29 -0400

    Added fontawesome icons in new post nav bar

--- frontend/components/dashboard/new_post_nav.jsx
+++ frontend/components/dashboard/new_post_nav.jsx
@@ -16,12 +16,12 @@ class NewPostNav extends React.Component {
   render() {
     return (
       <div className="post-form group">
-        <Link to="/new/text" className="post-form-link"><p>text</p></Link>
-        <Link to="/new/image" className="post-form-link"><p>image</p></Link>
-        <Link to="/new/audio" className="post-form-link"><p>audio</p></Link>
-        <Link to="/new/video" className="post-form-link"><p>video</p></Link>
-        <Link to="/new/link" className="post-form-link"><p>link</p></Link>
-        <Link to="/new/quote" className="post-form-link"><p>quote</p></Link>
+        <Link to="/new/text" className="post-form-link"><p><i className="fa fa-pencil" aria-hidden="true" /> text</p></Link>
+        <Link to="/new/image" className="post-form-link"><p><i className="fa fa-camera" aria-hidden="true" /> image</p></Link>
+        <Link to="/new/audio" className="post-form-link"><p><i className="fa fa-headphones" aria-hidden="true" /> audio</p></Link>
+        <Link to="/new/video" className="post-form-link"><p><i className="fa fa-video-camera" aria-hidden="true" /> video</p></Link>
+        <Link to="/new/link" className="post-form-link"><p><i className="fa fa-link" aria-hidden="true" /> link</p></Link>
+        <Link to="/new/quote" className="post-form-link"><p><i className="fa fa-quote-left" aria-hidden="true" /> quote</p></Link>
       </div>
     );
   }
