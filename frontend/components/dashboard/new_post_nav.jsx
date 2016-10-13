d07640114d4287113ec8c2abd34568dca0a7bcf3
Feather <featherpoirier@gmail.com>
2016-10-13 13:54:14 -0400

    Added color to new post icons

--- frontend/components/dashboard/new_post_nav.jsx
+++ frontend/components/dashboard/new_post_nav.jsx
@@ -16,12 +16,12 @@ class NewPostNav extends React.Component {
   render() {
     return (
       <div className="post-form group">
-        <Link to="/new/text" className="post-form-link"><p><i className="fa fa-pencil" aria-hidden="true" /> text</p></Link>
-        <Link to="/new/image" className="post-form-link"><p><i className="fa fa-camera" aria-hidden="true" /> image</p></Link>
-        <Link to="/new/audio" className="post-form-link"><p><i className="fa fa-headphones" aria-hidden="true" /> audio</p></Link>
-        <Link to="/new/video" className="post-form-link"><p><i className="fa fa-video-camera" aria-hidden="true" /> video</p></Link>
-        <Link to="/new/link" className="post-form-link"><p><i className="fa fa-link" aria-hidden="true" /> link</p></Link>
-        <Link to="/new/quote" className="post-form-link"><p><i className="fa fa-quote-left" aria-hidden="true" /> quote</p></Link>
+        <Link to="/new/text" className="post-form-link"><p><i className="fa fa-pencil red" aria-hidden="true" /> text</p></Link>
+        <Link to="/new/image" className="post-form-link"><p><i className="fa fa-camera orange" aria-hidden="true" /> image</p></Link>
+        <Link to="/new/audio" className="post-form-link"><p><i className="fa fa-headphones yellow" aria-hidden="true" /> audio</p></Link>
+        <Link to="/new/video" className="post-form-link"><p><i className="fa fa-video-camera green" aria-hidden="true" /> video</p></Link>
+        <Link to="/new/link" className="post-form-link"><p><i className="fa fa-link blue" aria-hidden="true" /> link</p></Link>
+        <Link to="/new/quote" className="post-form-link"><p><i className="fa fa-quote-left purple" aria-hidden="true" /> quote</p></Link>
       </div>
     );
   }
