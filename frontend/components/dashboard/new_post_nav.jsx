47476fe8630c1ad73f5211b35308406b82aca50d
Feather <featherpoirier@gmail.com>
2016-10-13 13:58:09 -0400

    More icon styling

--- frontend/components/dashboard/new_post_nav.jsx
+++ frontend/components/dashboard/new_post_nav.jsx
@@ -13,15 +13,16 @@ class NewPostNav extends React.Component {
 
   // refactor render method later to iterate over the post types
 
+
   render() {
     return (
       <div className="post-form group">
-        <Link to="/new/text" className="post-form-link"><p><i className="fa fa-pencil red" aria-hidden="true" /> text</p></Link>
-        <Link to="/new/image" className="post-form-link"><p><i className="fa fa-camera orange" aria-hidden="true" /> image</p></Link>
-        <Link to="/new/audio" className="post-form-link"><p><i className="fa fa-headphones yellow" aria-hidden="true" /> audio</p></Link>
-        <Link to="/new/video" className="post-form-link"><p><i className="fa fa-video-camera green" aria-hidden="true" /> video</p></Link>
-        <Link to="/new/link" className="post-form-link"><p><i className="fa fa-link blue" aria-hidden="true" /> link</p></Link>
-        <Link to="/new/quote" className="post-form-link"><p><i className="fa fa-quote-left purple" aria-hidden="true" /> quote</p></Link>
+        <Link to="/new/text" className="post-form-link"><p><i className="fa fa-pencil red" aria-hidden="true" /></p></Link>
+        <Link to="/new/image" className="post-form-link"><p><i className="fa fa-camera orange" aria-hidden="true" /></p></Link>
+        <Link to="/new/audio" className="post-form-link"><p><i className="fa fa-headphones yellow" aria-hidden="true" /></p></Link>
+        <Link to="/new/video" className="post-form-link"><p><i className="fa fa-video-camera green" aria-hidden="true" /></p></Link>
+        <Link to="/new/link" className="post-form-link"><p><i className="fa fa-link blue" aria-hidden="true" /></p></Link>
+        <Link to="/new/quote" className="post-form-link"><p><i className="fa fa-quote-left purple" aria-hidden="true" /></p></Link>
       </div>
     );
   }
