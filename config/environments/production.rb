51bc3b7e0fa0167dee169055e77250f60d0506f0
Feather <featherpoirier@gmail.com>
2017-01-16 15:06:49 -0500

    Reverted production env changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,10 +77,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-new webpack.DefinePlugin({
-  'process.env': {
-    NODE_ENV: JSON.stringify('production')
-  }
-}),
-new webpack.optimize.UglifyJsPlugin()
