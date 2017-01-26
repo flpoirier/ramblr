c9aabc5099bc46ab16563e486777f1d1ee40c815
Feather <featherpoirier@gmail.com>
2017-01-25 21:15:44 -0500

    Edited sidebar container to dispatch getFollows action:

--- frontend/components/dashboard/sidebar/sidebar_container.js
+++ frontend/components/dashboard/sidebar/sidebar_container.js
@@ -1,13 +1,16 @@
 import { connect } from 'react-redux';
 import { requestUsers } from '../../../actions/user_actions';
+import { getFollows } from '../../../actions/follow_actions';
 import Sidebar from './sidebar';
 
 const mapStateToProps = state => ({
   users: state.users,
+  follows: state.follows
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestUsers: () => dispatch(requestUsers())
+  requestUsers: () => dispatch(requestUsers()),
+  getFollows: () => dispatch(getFollows())
 });
 
 export default connect(
