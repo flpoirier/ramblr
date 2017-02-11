8457152eb4ac8acedf8cb51903ca5f03544a0871
Feather <featherpoirier@gmail.com>
2017-02-10 19:39:11 -0500

    Revert "edited assets config"
    This reverts commit bdd3f56805bf59c27447ee21a084479a0069d1b9.

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,6 @@ module Ramblr
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.serve_static_assets = true
-
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
