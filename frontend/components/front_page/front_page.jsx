2b6428a128cb337f8f395b518c1149448b04ab64
Feather <featherpoirier@gmail.com>
2017-04-11 15:16:41 -0400

    Updated list of images

--- frontend/components/front_page/front_page.jsx
+++ frontend/components/front_page/front_page.jsx
@@ -15,16 +15,11 @@ class FrontPage extends React.Component {
       "url(https://upload.wikimedia.org/wikipedia/commons/2/2a/Mission_Mountains_National_Bison_Range_Montana.jpg)",
       "url(https://static.pexels.com/photos/158607/cairn-fog-mystical-background-158607.jpeg)",
       "url(https://static.pexels.com/photos/39811/pexels-photo-39811.jpeg)",
-      "url(https://static.pexels.com/photos/128458/pexels-photo-128458.jpeg)",
       "url(https://static.pexels.com/photos/66874/landscape-meadow-field-mountains-66874.jpeg)",
       "url(https://static.pexels.com/photos/12567/photo-1444703686981-a3abbc4d4fe3.jpeg)",
       "url(https://static.pexels.com/photos/259474/pexels-photo-259474.jpeg)",
       "url(https://static.pexels.com/photos/207130/pexels-photo-207130.jpeg)",
-      "url(https://static.pexels.com/photos/132983/pexels-photo-132983.jpeg)",
-      "url(https://static.pexels.com/photos/29732/pexels-photo-29732.jpg)",
-      "url(https://static.pexels.com/photos/30360/pexels-photo-30360.jpg)",
       "url(https://static.pexels.com/photos/1440/city-road-street-buildings.jpg)",
-      "url(https://static.pexels.com/photos/6870/city-dark-new-york-bridge.jpg)"
       ]
     };
   }
@@ -52,7 +47,7 @@ class FrontPage extends React.Component {
     var random_img = this.state.images[Math.floor(Math.random() * this.state.images.length)];
     return {
       background: random_img,
-      "background-size": "cover"
+      backgroundSize: "cover"
     };
   }
 
