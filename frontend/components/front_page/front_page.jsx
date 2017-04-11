d1b1f3477beb7f09f3e9b635648e0dba68a43da2
Feather <featherpoirier@gmail.com>
2017-04-11 14:38:13 -0400

    Random image on front page

--- frontend/components/front_page/front_page.jsx
+++ frontend/components/front_page/front_page.jsx
@@ -7,6 +7,13 @@ class FrontPage extends React.Component {
   constructor(props) {
     super(props);
     this.guestLogin = this.guestLogin.bind(this);
+    this.state = { images: [
+      "url(https://static.pexels.com/photos/5439/earth-space.jpg)",
+      "url(https://static.pexels.com/photos/3853/sunny-sand-desert-hiking.jpeg)",
+      "url(https://static.pexels.com/photos/3247/nature-forest-industry-rails.jpg)",
+      "url(https://static.pexels.com/photos/6778/landscape-mountains-skyline-metal.jpg)",
+      "url(https://upload.wikimedia.org/wikipedia/commons/2/2a/Mission_Mountains_National_Bison_Range_Montana.jpg)" ]
+    };
   }
 
   guestLogin() {
@@ -15,6 +22,7 @@ class FrontPage extends React.Component {
 
   componentDidMount() {
     this.props.clearErrors();
+    this.getRandomBackground();
   }
 
   sessionLinks() {
@@ -28,9 +36,17 @@ class FrontPage extends React.Component {
     );
   }
 
+  getRandomBackground() {
+    var random_img = this.state.images[Math.floor(Math.random() * this.state.images.length)];
+    return {
+      background: random_img,
+      "background-size": "cover"
+    };
+  }
+
   render() {
     return (
-      <div className="front-page">
+      <div className="front-page" style={this.getRandomBackground()}>
         <span className="splash-parent">
             <div className="header-link"><h1>ramblr.</h1></div>
             {this.sessionLinks()}
